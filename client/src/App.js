6847c668ecb3c1bdc349a68adca826b073eee9f9
tarun bathwal <tarun.bathwal@gmail.com>
2018-08-29 13:02:52 +0530

    react bootstrapped

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email:'',
+            message:''
+        };
+    }
+
+    componentDidMount() {
+        axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
+        console.log("token"+localStorage.getItem('jwtToken'));
+        if(localStorage.getItem('jwtToken')==null)
+        {
+            this.props.history.push('/login');
+        }
+        axios.get('/')
+            .then(res => {
+                this.setState({message:"logged in"});
+            })
+            .catch((error) => {
+                if(error.response.status === 401) {
+                    this.props.history.push("/login");
+                }
+            });
+    }
+
+    logout = () => {
+        localStorage.removeItem('jwtToken');
+        window.location.reload();
+    }
+
+    render() {
+        return (
+            <div class="container">
+               <h1>{this.state.message}</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/Register.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+class Create extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            role:'',
+            ethaddress:''
+        };
+    }
+    onChange = (e) => {
+        const state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const { email, password,role,ethaddress } = this.state;
+
+        axios.post('/register', { email, password,role,ethaddress })
+            .then((result) => {
+                this.props.history.push("/login")
+            })
+            .catch(error=>{
+                if(error.response.status === 409) {
+                    this.setState({ message: "user already exists" });
+                }
+            });
+    }
+
+    render() {
+        const { email, password,role,ethaddress } = this.state;
+        return (
+            <div class="container">
+            <h1>{this.state.message}</h1>
+                <form class="form-signin" onSubmit={this.onSubmit}>
+                    <h2 class="form-signin-heading">Register</h2>
+                    <label for="inputEmail" class="sr-only">Email address</label>
+                    <input type="email" class="form-control" placeholder="Email address" name="email" value={email} onChange={this.onChange} required/>
+                    <label for="inputPassword" class="sr-only">Password</label>
+                    <input type="password" class="form-control" placeholder="Password" name="password" value={password} onChange={this.onChange} required/>
+                    <label for="inputRole" class="sr-only">Role</label>
+                    <input type="text" class="form-control" placeholder="role" name="role" value={role} onChange={this.onChange} required/>
+                    <label for="inputAddress" class="sr-only">Eth Address</label>
+                    <input type="text" class="form-control" placeholder="Eth address" name="ethaddress" value={ethaddress} onChange={this.onChange} required/>
+                    
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Create;
